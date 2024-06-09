b64fd18760cbb15a28a1e367fa336c59c7de373d
rajascreamo <rajascreamo04@gmail.com>
2024-06-09 21:41:15 +0800

    remake user model database + seeder for admin

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,12 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->string('NIM')->primary();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('address')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('user_type')->default('user');
+            $table->string('user_type')->default('mahasiswa');
             $table->rememberToken();
             $table->timestamps();
         });
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +17,11 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'NIM' => '1',
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('admin1234'),
+            'user_type' => 'admin',
         ]);
     }
 }
